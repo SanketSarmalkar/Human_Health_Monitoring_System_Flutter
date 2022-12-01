f10fb8a0d5481ef8896dd196eed595131eca9765
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2022-12-01 18:26:20 +0530

    solved ajax problem

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 var jsonData;
+var bpm;
+var irvalue;
+var spO2;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,6 +16,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+/*
+  @override
+  void initState() {
+    super.initState();
+    getpulse();
+  }
+*/
   // https://api.thingspeak.com/channels/1964229/fields/1.json?api_key=TD3ZH96RZW9P04AG&results=2
   Future getpulse() async {
     var response = await http.get(Uri.https(
@@ -21,14 +31,19 @@ class _HomePageState extends State<HomePage> {
         {'api_key': 'TD3ZH96RZW9P04AG', 'results': '1'}));
     //print(response.body);
     //setState(){
-      jsonData = jsonDecode(response.body);
+    jsonData = jsonDecode(response.body);
     //}
     print(jsonData['feeds'][0]['field3']);
+    setState(() {
+      bpm = jsonData['feeds'][0]['field1'];
+      irvalue = jsonData['feeds'][0]['field2'];
+      spO2 = jsonData['feeds'][0]['field3'];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    getpulse();
+    //getpulse();
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -43,13 +58,13 @@ class _HomePageState extends State<HomePage> {
       body: FutureBuilder(
         future: getpulse(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
+          //if (snapshot.connectionState == ConnectionState.done) {
             return Container(
               padding: EdgeInsets.all(8),
               child: Column(
                 children: [
                   Expanded(
-                    flex: 1,
+                    flex: 2,
                     child: Padding(
                       padding: const EdgeInsets.all(16.0),
                       child: Container(
@@ -58,7 +73,8 @@ class _HomePageState extends State<HomePage> {
                             borderRadius: BorderRadius.circular(25)),
                         child: Center(
                             child: Text(
-                          "BPM : " + (jsonData["feeds"][0]["field1"]).toString(),
+                          "BPM : " +
+                              (bpm).toString(),
                           style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
@@ -68,7 +84,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   Expanded(
-                    flex: 1,
+                    flex: 2,
                     child: Padding(
                       padding: const EdgeInsets.all(16.0),
                       child: Container(
@@ -78,7 +94,7 @@ class _HomePageState extends State<HomePage> {
                         child: Center(
                             child: Text(
                           "IR Value : " +
-                              jsonData["feeds"][0]["field2"].toString(),
+                              irvalue.toString(),
                           style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
@@ -88,7 +104,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   Expanded(
-                    flex: 1,
+                    flex: 2,
                     child: Padding(
                       padding: const EdgeInsets.all(16.0),
                       child: Container(
@@ -97,7 +113,8 @@ class _HomePageState extends State<HomePage> {
                             borderRadius: BorderRadius.circular(25)),
                         child: Center(
                             child: Text(
-                          "SpO2 : " + (jsonData["feeds"][0]["field3"]).toString(),
+                          "SpO2 : " +
+                              (spO2).toString(),
                           style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
@@ -105,17 +122,26 @@ class _HomePageState extends State<HomePage> {
                         )),
                       ),
                     ),
-                  ),
+                  ),/*
+                  Expanded(
+                    flex: 1,
+                      child:GestureDetector(
+                        onTap: (){
+                          getpulse();
+                        },
+                        child: Icon(Icons.restart_alt_rounded,size: 55,),
+                        ),
+                      )*/
                 ],
               ),
             );
-          } else {
+          //} /*else {
             return Container(
               child: Center(
                 child: CircularProgressIndicator(),
               ),
             );
-          }
+          //}
         },
       ),
     );
