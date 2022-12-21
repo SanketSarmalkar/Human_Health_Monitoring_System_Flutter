9e5c4e6e7631c1b48fbc7328849ff71ca55e5766
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2022-12-21 23:01:03 +0530

    done with frontend

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -32,13 +32,18 @@ class _HomePageState extends State<HomePage> {
     //print(response.body);
     //setState(){
     jsonData = jsonDecode(response.body);
+
+    var posting = await http.post(Uri.https(
+        'api.thingspeak.com',
+        'channels/1964229/feeds.json',
+        {'api_key': 'TD3ZH96RZW9P04AG', 'results': '1'}));
     //}
     print(jsonData['feeds'][0]['field3']);
-    setState(() {
-      bpm = jsonData['feeds'][0]['field1'];
-      irvalue = jsonData['feeds'][0]['field2'];
-      spO2 = jsonData['feeds'][0]['field3'];
-    });
+    // setState(() {
+    //   bpm = jsonData['feeds'][0]['field1'];
+    //   irvalue = jsonData['feeds'][0]['field2'];
+    //   spO2 = jsonData['feeds'][0]['field3'];
+    // });
   }
 
   @override
@@ -56,94 +61,102 @@ class _HomePageState extends State<HomePage> {
         toolbarHeight: 100,
       ),
       body: FutureBuilder(
-        future: getpulse(),
-        builder: (context, snapshot) {
-          //if (snapshot.connectionState == ConnectionState.done) {
-            return Container(
-              padding: EdgeInsets.all(8),
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.deepPurpleAccent,
-                            borderRadius: BorderRadius.circular(25)),
-                        child: Center(
-                            child: Text(
-                          "BPM : " +
-                              (bpm).toString(),
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30),
-                        )),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.deepPurpleAccent,
-                            borderRadius: BorderRadius.circular(25)),
-                        child: Center(
-                            child: Text(
-                          "IR Value : " +
-                              irvalue.toString(),
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30),
-                        )),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Colors.deepPurpleAccent,
-                            borderRadius: BorderRadius.circular(25)),
-                        child: Center(
-                            child: Text(
-                          "SpO2 : " +
-                              (spO2).toString(),
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30),
-                        )),
-                      ),
-                    ),
-                  ),/*
-                  Expanded(
-                    flex: 1,
-                      child:GestureDetector(
-                        onTap: (){
-                          getpulse();
-                        },
-                        child: Icon(Icons.restart_alt_rounded,size: 55,),
+          future: getpulse(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.deepPurpleAccent,
+                              borderRadius: BorderRadius.circular(25)),
+                          child: Center(
+                              child: Text(
+                            "BPM : " + (bpm).toString(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          )),
                         ),
-                      )*/
-                ],
-              ),
-            );
-          //} /*else {
-            return Container(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          //}
-        },
-      ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.deepPurpleAccent,
+                              borderRadius: BorderRadius.circular(25)),
+                          child: Center(
+                              child: Text(
+                            "IR Value : " + irvalue.toString(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          )),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.deepPurpleAccent,
+                              borderRadius: BorderRadius.circular(25)),
+                          child: Center(
+                              child: Text(
+                            "SpO2 : " + (spO2).toString(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30),
+                          )),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            bpm = jsonData['feeds'][0]['field1'];
+                            irvalue = jsonData['feeds'][0]['field2'];
+                            spO2 = jsonData['feeds'][0]['field3'];
+                          });
+                        },
+                        child: Icon(
+                          Icons.restart_alt_rounded,
+                          size: 55,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: MaterialButton(
+                      color: Colors.blueGrey,
+                      onPressed: () {},
+                    ))
+                  ],
+                ),
+              );
+            } else {
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+          }),
     );
   }
 }
